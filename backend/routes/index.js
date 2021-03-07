cb8ecac5177d510d0050ef400d6cc9649b002ffa
Sergio Ernesto Rivera Rios <negrillo@MacBook-Pro.local>
2021-03-06 23:20:50 -0700

    added csv function

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,26 @@
 //import express and router
+
+import fs from 'fs'
 import express from "express"
 const router = express.Router()
 
+//import csv and fs
+import csv from 'csv-parser'
+const results = [];
+
+const filepath = '/Users/negrillo/Desktop/nodejs/500albums/backend/albums/albumlist.csv';
+
 router.get('/', (req, res) => {
-    console.log("hey")
-    res.json("hey")
+    fs.createReadStream(filepath)
+        .pipe(csv())
+        .on('data', (data) => results.push(data))
+        .on('end', () => {
+            res.json(results)
+                // [
+                //   { NAME: 'Daffy Duck', AGE: '24' },
+                //   { NAME: 'Bugs Bunny', AGE: '22' }
+                // ]
+        });
 })
 
 export default router;
