6f658f2e8beadc06118f8422680099265f30ed54
Sergio Ernesto Rivera Rios <negrillo@MacBook-Pro.local>
2021-03-06 22:21:08 -0700

    added backend

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+
+//Main server
+const app = express()
+const PORT = process.env.PORT || 5000
+
+//Additional config
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors())
+
+app.listen(PORT, () => console.log(`Running on server ${PORT}`))
