9592a8877c659ca5f42ba66e74e58770b2fa460c
Sergio Ernesto Rivera Rios <negrillo@MacBook-Pro.local>
2021-03-06 23:21:04 -0700

    added route

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 
+import routes from './routes/index.js'
 //Main server
 const app = express()
 const PORT = process.env.PORT || 5000
@@ -10,5 +11,7 @@ const PORT = process.env.PORT || 5000
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
+
+app.use('/', routes)
 
 app.listen(PORT, () => console.log(`Running on server ${PORT}`))
